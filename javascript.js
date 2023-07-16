59760dad07e84e3c03565057e7784834f2f5802f
GoodGuyNaiv <naivedhyap@gmail.com>
2023-07-17 02:21:03 +0530

    final commit message

--- javascript.js
+++ javascript.js
@@ -9,23 +9,23 @@ userChoice = userChoice.toLowerCase();
 let computerSelection = getComputerChoice();
 
 if (userChoice == "rock" && computerSelection == "paper") {
-  console.log("you lose");
+  console.log("Opponent chose paper , hence you lose");
 } else if (userChoice == "rock" && computerSelection == "scissors") {
-  console.log("you lose");
+  console.log("Opponent chose scissors , hence you win");
 } else if (userChoice == "rock" && computerSelection == "rock") {
-  console.log("you lose");
+  console.log("Opponent chose rock too , hence its a tie");
 } else if (userChoice == "paper" && computerSelection == "paper") {
-  console.log("you lose");
+  console.log("Opponent chose paper too , hence its a tie");
 } else if (userChoice == "paper" && computerSelection == "rock") {
-  console.log("you lose");
+  console.log("Opponent chose rock , hence you win");
 } else if (userChoice == "paper" && computerSelection == "scissors") {
-  console.log("you lose");
+  console.log("Opponent chose scissors , hence you lose");
 } else if (userChoice == "scissors" && computerSelection == "paper") {
-  console.log("you lose");
+  console.log("Opponent chose paper , hence you win");
 } else if (userChoice == "scissors" && computerSelection == "rock") {
-  console.log("you lose");
-} else if (userChoice == "credfscissors" && computerSelection == "scissors") {
-  console.log("you lose");
+  console.log("Opponent chose rock , hence you lose");
+} else if (userChoice == "scissors" && computerSelection == "scissors") {
+  console.log("Opponent chose scissors too , hence its a tie");
 } else {
   console.log("invalid input");
 }
